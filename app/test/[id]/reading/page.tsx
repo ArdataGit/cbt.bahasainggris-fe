012ad57f214ec8ec883da57b9af6ec3f3094c29b
yudayuda78 <85638796+yudayuda78@users.noreply.github.com>
2026-03-07 12:34:05 +0700

    Fix : play

--- app/test/[id]/reading/page.tsx
+++ app/test/[id]/reading/page.tsx
@@ -443,7 +443,7 @@ export default function ReadingTestPage() {
                     stroke="currentColor" 
                     viewBox="0 0 24 24"
                   >
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="9 5l7 7-7 7" />
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7" />
                   </svg>
                 </button>
               ) : (
